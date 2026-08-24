fcb73d698f768afdc3a784dc35da99abd3977b56
ImperialDave <148258480+ImperialDave@users.noreply.github.com>
2026-08-24 05:53:46 -0700

    Glyphbound levels: spicy pits with edge teeth, residual ink trenches, saw floors, bounce islands; new room kinds teethpit/inktrench/sawgap/fanwell/crushgap

--- src/glyphbound/levels.ts
+++ src/glyphbound/levels.ts
@@ -1,1 +1,9 @@
-PLACEHOLDER
+import { DISTRICTS } from "./districts";
+import type { LevelId, TaskDef, ThemeId } from "./types";
+import { FIRST_BOOK, STAGE_COUNT } from "./types";
+
+export type { LevelId, ThemeId };
+export { STAGE_COUNT, FIRST_BOOK };
+
+// TEMP - will be replaced
+export const LEVELS = {};
